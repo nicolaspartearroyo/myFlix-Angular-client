c3b78eb86571e0beb851f390fc222cc2da4ede29
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-09-02 01:09:35 +0200

    login view created

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -33,10 +33,12 @@ export class UserRegistrationFormComponent implements OnInit {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
       // Logic for a successful user registration goes here! (To be implemented)
       this.dialogRef.close(); // This will close the modal on success!
+      console.log(result);
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
     }, (result) => {
+      console.log(result);
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
